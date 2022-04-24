e83321d4d019d6594d6b26ad1fbbed11de2d18d8
Temirlan <bamblebee988@gmail.com>
2022-04-24 12:07:57 +0600

    [step 6]: remove conditions for correct answer

--- brain_games/calc_game.py
+++ brain_games/calc_game.py
@@ -20,11 +20,6 @@ def game_step():
     title = 'What is the result of the expression?'
     question = "{n1} {op} {n2}".format(n1=num1, n2=num2, op=operation)
     answer = get_answer(question, title)
-    if operation == '+':
-        correct_answer = num1 + num2
-    elif operation == '-':
-        correct_answer = num1 - num2
-    else:
-        correct_answer = num1 * num2
+    correct_answer = eval(question)
 
     return int(answer), correct_answer
